f2e05022d66b23ef19acaa0eb9c27cdf363e4e6f
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-03-02 11:25:00 +0530

    initial commit

---
+++ OOP Concepts/intro-to-OOP.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// Class Definition
+class Student {
+public:
+    string name;
+    int age;
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main() {
+    // Object Creation
+    Student s1;
+
+    // Assign values
+    s1.name = "Vikash";
+    s1.age = 20;
+
+    // Call function
+    s1.display();
+
+    return 0;
+}
